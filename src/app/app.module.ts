e6605bd5f3d5df60a3132f886bc32358f1e6c8e0
mdimp <dinakar@outlook.in>
2018-12-15 20:35:28 +0530

    Routes implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponentComponent } from './page-not-found-component/page-not-found-component.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+const appRoutes: Routes = [
+  { path: 'about', component: AboutComponent },
+  {path: 'home' , component: HomeComponent},
+
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+
+ 
+ 
+ 
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    PageNotFoundComponentComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
